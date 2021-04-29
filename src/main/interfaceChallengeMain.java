56b09fd0cdb63a18273a279e85f25cc796f7df79
Bartosz Slesar <bartoszslesar@gmail.com>
2021-04-29 21:10:30 +0200

    Added Resolved Challenge from Java Masterclass

--- src/main/interfaceChallengeMain.java
+++ src/main/interfaceChallengeMain.java
@@ -2,20 +2,17 @@ package main;
 
 import interfaceChallengeSaveable.ISaveable;
 import interfaceChallengeSaveable.ReadWrite.ReadObjectFromFile;
-import interfaceChallengeSaveable.ReadWrite.SaveObjectToFile;
 import interfaceChallengeSaveable.Truck;
-import interfaceChallengeSaveable.utils.ReadWriteObject;
 
-import java.util.List;
 
 public class interfaceChallengeMain {
 
-    public static void main(String[] args) throws ClassNotFoundException {
+    public static void main(String[] args){
 
 //        ISaveable saveable = new Truck("Mercedes", 8, false, "Red", "Tank Truck");
 //        SaveObjectToFile.saveToFile(saveable, "testFileName");
-        ISaveable truck = ReadObjectFromFile.readObject(new Truck(), "testFileName");
-        System.out.println(truck.toString());
+//        ISaveable truck = ReadObjectFromFile.readObject(new Truck(), "testFileName");
+//        System.out.println(truck.toString());
 
 
     }
